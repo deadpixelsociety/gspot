a6df44a38307cef891c5850e1ab367a84eaca8f3
Matt Sams <matt@thedeadpixelsociety.com>
2024-06-01 10:24:37 -0400

    Fixed missed client name reference

--- addons/gspot/messages/gsrequest_server_info.gd
+++ addons/gspot/messages/gsrequest_server_info.gd
@@ -5,5 +5,5 @@ class_name GSRequestServerInfo
 func _init(message_id: int) -> void:
 	super._init(message_id)
 	message_type = MESSAGE_TYPE_REQUEST_SERVER_INFO
-	fields[MESSAGE_FIELD_CLIENT_NAME] = GSClient.CLIENT_NAME
+	fields[MESSAGE_FIELD_CLIENT_NAME] = GSClient.get_client_string()
 	fields[MESSAGE_FIELD_MESSAGE_VERSION] = GSClient.MESSAGE_VERSION
